00f837486127155f259bb1e965d8bfca80e16a2d
Jotave <joaovitorkrugerdias@gmail.com>
2026-07-24 12:14:12 -0300

    chore: rebuild e atualização do sistema

--- modules/home-config/bash.nix
+++ modules/home-config/bash.nix
@@ -4,7 +4,7 @@
     programs.bash = {
       enable = true;
       shellAliases = {
-        update = "sudo nixos-rebuild switch";
+        rebuild = "cd /etc/nixos && git add . && sudo nixos-rebuild switch --flake . && (git diff-index --quiet HEAD || (git commit -m \"chore: rebuild e atualização do sistema\" && git push))";
         config = "sudo nano /etc/nixos/configuration.nix";
       };
       initExtra = "fastfetch";
